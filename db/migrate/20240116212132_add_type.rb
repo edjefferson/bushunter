29b78728718b336f5dc6b22bced91688b3a3d3f2
Ed Jefferson <e.jefferson@gmail.com>
2024-01-16 21:23:17 +0000

    type

---
+++ db/migrate/20240116212132_add_type.rb
@@ -0,0 +1,17 @@
+class AddType < ActiveRecord::Migration[7.1]
+  def up
+    execute <<~SQL
+
+      CREATE TYPE update_type as ("naptanId" text, "stationName" text, "lineName" text, "platformName" text, "destinationName" text, "vehicleId" text, "expectedArrival" timestamp, timestamp timestamp);
+
+      
+    SQL
+    
+  end
+
+  def down
+    execute "DROP TYPE update_type"
+  end
+end
+
+  
